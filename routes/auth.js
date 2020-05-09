f921eebfba0a6143a4150e26af8a98e27399c7bf
OswaldHe <zifanhe1202@ucla.edu>
2020-05-08 20:12:28 -0700

    add signup endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -8,9 +8,17 @@ let db = new sqlite3.Database('./webuy.db', sqlite3.OPEN_READWRITE, (err)=>{
     }else{
         console.log("Connected to sqlite db...");
     }
-})
+});
 
-let sql = `SELECT DISTINCT username, email FROM User WHERE username = ? AND password = ?`;
+let counter;
+db.get(`SELECT COUNT() AS user_count FROM User`, (err, row)=>{
+    if(err){
+        console.error(err.message);
+    }else{
+        counter = row.user_count;
+        console.log(counter);
+    }
+})
 
 router.get('/', function(req, res) {
     res.send('WeBuy API 1.0');
@@ -23,6 +31,7 @@ router.post('/login', function(req, res) {
         res.status(400).send("Please enter your username and password.");
         res.end();
     }
+    let sql = `SELECT DISTINCT username, email FROM User WHERE username = ? AND password = ?`;
     db.get(sql, [username, password], (err, row) =>{
         if(err){
             console.error(err.message);
@@ -34,6 +43,34 @@ router.post('/login', function(req, res) {
             res.status(400).send("Invalid username and password!");
         }
         res.end();
+    })
+});
+
+router.post('/signup', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+
+    let find = `SELECT DISTINCT username FROM User WHERE username = ?`;
+    let add = `INSERT INTO User(user_id, username, email, password) VALUES(?, ?, ?, ?)`;
+    db.get(find, [username], (err, row)=>{
+        if(err){
+            return console.log(err.message);
+        }
+        if(!row){
+            console.log("Adding the account into db...");
+            db.run(add, [counter, username, email, password], (err)=>{
+                if(err){
+                    return console.log(err.message);
+                }else{
+                    res.status(200).send(`User ${counter} has been added.`);
+                    console.log(`User ${counter} has been added.`);
+                    counter++;
+                }
+            })
+        }else{
+            res.status(201).send("user is already existed");
+        }
     })
 });
 
