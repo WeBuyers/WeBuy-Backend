2ad13fb6004bd04a02c60272d109ce84710dee24
OswaldHe <zifanhe1202@ucla.edu>
2020-05-08 19:24:02 -0700

    connect with sqlite db

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('./webuy.db', sqlite3.OPEN_READWRITE, (err)=>{
+    if(err){
+        console.error(err.message);
+    }else{
+        console.log("Connected to sqlite db...");
+    }
+})
+
+let sql = `SELECT DISTINCT username, email FROM User WHERE username = ? AND password = ?`;
 
 router.get('/', function(req, res) {
     res.send('WeBuy API 1.0');
@@ -8,18 +19,22 @@ router.get('/', function(req, res) {
 router.post('/login', function(req, res) {
     const username = req.body.username;
     const password = req.body.password;
-
-    if(username && password){
-        if(username === "usr" && password === "123"){
-            res.status(200).send("logged in...");
+    if(!username || !password){
+        res.status(400).send("Please enter your username and password.");
+        res.end();
+    }
+    db.get(sql, [username, password], (err, row) =>{
+        if(err){
+            console.error(err.message);
+        }
+        if(row){
+            res.status(200).send(`logged in... ${row.username}`);
+            console.log("Successfully logged in!");
         }else{
             res.status(400).send("Invalid username and password!");
         }
         res.end();
-    }else{
-        res.status(400).send("Please enter your username and password.");
-        res.end();
-    }
+    })
 });
 
 module.exports = router;
