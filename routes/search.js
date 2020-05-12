32dd88559b627c7f81f84e0d1a75916458791764
OswaldHe <zifanhe1202@ucla.edu>
2020-05-12 16:40:24 -0700

    change from raw sqlite to sequelize orm

--- routes/search.js
+++ routes/search.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const sqlite3 = require('sqlite3').verbose();
+const Sequelize = require('sequelize');
 
-//connecting to sqlite database
-let db = new sqlite3.Database('./webuy.db', sqlite3.OPEN_READWRITE, (err)=>{
-    if(err){
-        console.error(err.message);
-    }else{
-        console.log("Connected to sqlite db...");
-    }
+const sequelize = new Sequelize({
+   dialect: "sqlite",
+   storage: "./webuy.db"
 });
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 //return a list of all items that can be searched
 router.get("/allitem", function (req, res) {
