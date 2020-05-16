08f70dba0b7ae8273daaf94342290300a5e0e4e7
Violet Guo <50038650+BingcuiGuo@users.noreply.github.com>
2020-05-16 15:01:07 -0700

    changed the database structure and fill out the store information

--- routes/search.js
+++ routes/search.js
@@ -7,83 +7,147 @@ const sequelize = new Sequelize({
    storage: "./webuy.db"
 });
 
-//create the three tables that we need 
-const itemlist = sequelize.define('Items',
-{
-    itemname:{
-        type: Sequelize.STRING, 
-        allowNull: false
-    },
-    picturelink: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
-}, {freezeTableName: true, timestamps: false});
+// //create the three tables that we need 
+// const itemlist = sequelize.define('Items',
+// {
+//     itemname:{
+//         type: Sequelize.STRING, 
+//         allowNull: false
+//     },
+//     picturelink: {
+//         type: Sequelize.STRING,
+//         allowNull: false
+//     },
+//     storeid:{
+//         type: Sequelize.INTEGER,
+//         allowNull: false
+//     },
+//     price:{
+//         type:Sequelize.DOUBLE,
+//         allowNull: false
+//     }
+// }, {freezeTableName: true, timestamps: false});
 
 
-(async () => {
-    await sequelize.sync();
-    const a = await itemlist.create({
-        itemname: 'apple',
-        picturelink: "good"
-    });
-    console.log('------------------------------------------')
-console.log(a.toJSON());
-})(); 
+// (async () => {
+//     await sequelize.sync();
+//     const a = await itemlist.create({
+//         itemname: 'apple',
+//         picturelink: "good",
+//         storeid:1,
+//         price: 28.5
+//     });
+// console.log(a.toJSON());
+// })(); 
 
-(async () => {
-    await sequelize.sync();
-    const a = await itemlist.create({
-        itemname: 'pear',
-        picturelink: "bad"
-    });
-console.log(a.toJSON());
-})(); 
+// (async () => {
+//     await sequelize.sync();
+//     const a = await itemlist.create({
+//         itemname: 'pear',
+//         picturelink: "bad",
+//         storeid:2,
+//         price:30
+//     });
+// console.log(a.toJSON());
+// })(); 
 
-(async () => {
-    await sequelize.sync();
-    const a = await itemlist.create({
-        itemname: 'banana',
-        picturelink: "git"
-    });
-console.log(a.toJSON());
-})();
+// (async () => {
+//     await sequelize.sync();
+//     const a = await itemlist.create({
+//         itemname: 'banana',
+//         picturelink: "git",
+//         storeid:3,
+//         price:5
+//     });
+// console.log(a.toJSON());
+// })();
 
-const store = sequelize.define('Stores',
-{
-    storename:{
-        type: Sequelize.STRING, 
-        allowNull: false
-    },
-    latitude: {
-        type: Sequelize.DOUBLE,
-        allowNull: false
-    },
-    longitude: {
-        type: Sequelize.DOUBLE,
-        allowNull: false
-    }
-}, {freezeTableName: true, timestamps: false});
+// const store = sequelize.define('Stores',
+// {
+//     storename:{
+//         type: Sequelize.STRING, 
+//         allowNull: false
+//     },
+//     latitude: {
+//         type: Sequelize.DOUBLE,
+//         allowNull: false
+//     },
+//     longitude: {
+//         type: Sequelize.DOUBLE,
+//         allowNull: false
+//     }
+// }, {freezeTableName: true, timestamps: false});
+
+// (async () => {
+//     await sequelize.sync();
+//     const a = await store.create({
+//         storename:"Target Westwood",
+//         latitude:34.0627963,
+//         longitude: -118.4460309
+//     });
+// console.log(a.toJSON());
+// })();
+
+// (async () => {
+//     await sequelize.sync();
+//     const a = await store.create({
+//         storename:"Trader Joes Westwood",
+//         latitude:34.0622696,
+//         longitude: -118.4459629
+//     });
+// console.log(a.toJSON());
+// })();
+
+// (async () => {
+//     await sequelize.sync();
+//     const a = await store.create({
+//         storename:"Ralphs Westwood",
+//         latitude:34.062651,
+//         longitude: -118.4461308
+//     });
+// console.log(a.toJSON());
+// })();
 
 
-const relationship = sequelize.define('Relationships',
-{
-    itemname:{
-        type: Sequelize.STRING, 
-        allowNull: false
-    },
-    store: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
-}, {freezeTableName: true, timestamps: false});
+// (async () => {
+//     await sequelize.sync();
+//     const a = await store.create({
+//         storename:"Nijiya Markey Sawtelle",
+//         latitude:33.9197299,
+//         longitude: -118.4810098
+//     });
+// console.log(a.toJSON());
+// })();
 
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-        return sequelize.sync({ force:true }); 
-    })
+
+// (async () => {
+//     await sequelize.sync();
+//     const a = await store.create({
+//         storename:"Nijiya Markey Sawtelle",
+//         latitude:33.9197299,
+//         longitude: -118.4810098
+//     });
+// console.log(a.toJSON());
+// })();
+
+// const relationship = sequelize.define('Relationships',
+// {
+//     itemname:{
+//         type: Sequelize.STRING, 
+//         allowNull: false
+//     },
+//     store: {
+//         type: Sequelize.STRING,
+//         allowNull: false
+//     }
+// }, {freezeTableName: true, timestamps: false});
+
+// sequelize
+//     .authenticate()
+//     .then(() => {
+//         console.log('Connection has been established successfully.');
+//         return sequelize.sync({ }); 
+//     })
 
 
 //return a list of all items that can be searched
