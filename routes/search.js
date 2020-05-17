339eacb4f9dc0e1dd367b2735432bdcc82cd770b
Violet Guo <50038650+BingcuiGuo@users.noreply.github.com>
2020-05-16 22:58:28 -0700

    change the distance calculation

--- routes/search.js
+++ routes/search.js
@@ -44,7 +44,7 @@ router.get("/item", async function (req, res) {
                 .then(data =>{
                     if(data.length !== 0){
                         const store_matched = data.dataValues;
-                        items[i].distance = calculate_distance(current_lat, store_matched.latitude, current_lon, store_matched.longitude);
+                        items[i].distance = getDistanceFromLatLonInKm(cureent_lat,current_lon,store_matched.latitude,store_matched.longitude);
                     }
                 }).catch(err=>{console.error(err.message)})
             promise.push(newPromise);
@@ -65,8 +65,23 @@ router.post("/itemlist", async function (req, res) {
     {/*todo*/}
 })
 
-function calculate_distance(lat1, lat2, lon1, lon2) {
-    return Math.sqrt((lat1-lat2)*(lat1-lat2)+(lon1-lon2)*(lon1-lon2)); 
-}
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+  }
+  
+  function deg2rad(deg) {
+    return deg * (Math.PI/180)
+  }
 
 module.exports = router;
